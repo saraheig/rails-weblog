1186ccad9e1b07495affe0a913ab3e1944cda11b
Thibaud <thibaud@thibaud.gg>
2017-06-08 17:04:58 +0000

    User#display_name test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#display_name when admin' do
+    # user = User.new(admin: true, name: 'bob')
+    user = users(:bob)
+    assert_equal user.name + ' (admin)', user.display_name
+  end
+  
+  test '#display_name when not admin' do
+    # user = User.new(admin: false, name: 'jane')
+    user = users(:jane)
+    assert_equal user.name, user.display_name
+  end
 end
